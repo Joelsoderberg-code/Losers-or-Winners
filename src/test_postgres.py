8ba82f55dd20ef237481aec842b8d2345a3e850d
Joel Söderberg <joel_soderberg@hotmail.com>
2025-09-17 14:24:40 +0200

    chore: ta bort testfiler

--- src/test_postgres.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-conn = psycopg2.connect(host="localhost", port=5432, dbname="stocks", user="postgres", password="password")
-cur = conn.cursor()
-cur.execute("SELECT version();")
-version = cur.fetchone()
-print("Postgres version:", version)
-cur.close()
-conn.close()
